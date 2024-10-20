5184397bed74ce82ad5c7e4fbcadf07e859c69e4
Skyiesac <jainsachi1202@gmail.com>
2024-10-20 23:35:23 +0530

    CRUD featured added

--- notesmain/urls.py
+++ notesmain/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('notes/', views.showlist),
-    path('notes/<str:pk>', views.details),
+    path('notes/', views.showlist , name='gotonote'),
+    path('notes/<str:pk>', views.details, name='nodetail'),
+     path('notes/new/', views.create_note, name='create_note'),
+    path('notes/edit/<str:pk>/', views.editnote, name='editnote'),  # URL for editing a note
+    path('notes/<str:pk>/delete/', views.deletenote, name='deletenote'),
 ]
--- notesmain/views.py
+++ notesmain/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404, redirect
 from .models import Notes
 from datetime import datetime
-
+from .forms import Newnotes
 
 # Create your views here.
 def showlist(request):
@@ -11,3 +11,33 @@ def showlist(request):
 def details(request, pk):
     note = Notes.objects.get(pk=pk)
     return render(request, 'notesmain/brief.html', {'note': note})
+
+def create_note(request):
+    if request.method == 'POST':
+        form = Newnotes(request.POST)  
+        if form.is_valid():
+            form.save() 
+            return redirect('gotonote') 
+    else:
+        form = Newnotes() 
+    return render(request, 'notesmain/create_note.html', {'form': form})
+
+def editnote(request,pk):
+    note = get_object_or_404(Notes, pk=pk)
+    form = Newnotes(instance=note)
+    if request.method == 'POST':
+        form = Newnotes(request.POST, instance= note)
+        if form.is_valid():
+            form.save()
+            return redirect('nodetail',pk=pk)
+        else:
+          form = Newnotes(instance=note)
+    return render(request, 'notesmain/edit.html',{'form':form})
+
+def deletenote(request,pk):
+    note = get_object_or_404(Notes, pk=pk)
+
+    if request.method == 'POST':
+        note.delete()
+        return redirect('gotonote')
+    return render(request, 'notesmain/delete.html', {'note':note})
