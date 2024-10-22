9ae1e6820db8871b2b64576287c4c0e1f9d6ae02
Skyiesac <jainsachi1202@gmail.com>
2024-10-23 00:02:24 +0530

    some minor issues solved

--- api/urls.py
+++ api/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
       path('', views.getData),
       path('add/', views.addData),
       path('note/<int:id>',views.detailData),
-      path('api/register/', views.RegisterView.as_view(), name='register'),
-      path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-      path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+      path('register/', views.RegisterView.as_view(), name='toregister'),
+      path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+      path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     ]
 
